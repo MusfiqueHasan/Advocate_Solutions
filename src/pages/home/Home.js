f9fcb7614543825d0113455f40f19d94c91a5575
Musfique Hasan <musfique.hasan.professional@gmail.com>
2022-09-25 23:23:37 +0600

    folder structure done

---
+++ src/pages/home/Home.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import Attorneys from './attorneys/Attorneys'
+import Footer from './footer/Footer'
+import Header from './header/Header'
+import Testimonial from './testimonial/Testimonial'
+
+const Home = () => {
+    return (
+        <>
+            <Header />
+            <Attorneys />
+            <Testimonial />
+            <Footer />
+        </>
+    )
+}
+
+export default Home
