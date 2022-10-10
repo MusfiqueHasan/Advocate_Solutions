58fb11a595d534b0a9e1a9f88a854e6c9a1b69ac
ebrahim <ebrahimislam34@gmail.com>
2022-10-10 15:22:07 +0600

    consultant category added

---
+++ src/pages/consultation/CategoryWiseAdvocate/CategoryWiseAdvocate.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const CategoryWiseAdvocate = () => {
+  return (
+    <div>
+
+
+    </div>
+  )
+}
+
+export default CategoryWiseAdvocate
--- src/pages/consultation/Consultation.js
+++ src/pages/consultation/Consultation.js
@@ -1,9 +1,260 @@
-import React from 'react'
-
+import React from "react";
+import ArrowRightAltIcon from "@mui/icons-material/ArrowRightAlt";
+import cat1 from "../../Assets/icons/cat1.png";
+import cat3 from "../../Assets/icons/cat3.png";
+import cat4 from "../../Assets/icons/cat4.png";
+import cat5 from "../../Assets/icons/cat5.png";
+import cat6 from "../../Assets/icons/cat6.png";
+import cat7 from "../../Assets/icons/cat7.png";
+import cat8 from "../../Assets/icons/cat8.png";
+import cat9 from "../../Assets/icons/cat9.png";
+import cat10 from "../../Assets/icons/cat10.png";
+import womentcat1 from "../../Assets/icons/womentcat1.png";
+import womentcat2 from "../../Assets/icons/womentcat2.png";
+import womentcat3 from "../../Assets/icons/womentcat3.png";
+import { Link } from "react-router-dom";
 const Consultation = () => {
   return (
-    <div>Consultation</div>
-  )
-}
+    <div className=" p-10">
+      <div>
+        <h1 className=" text-4xl font-bold italic text-gray-800 hober:text-gold">
+          Please select a speciality
+        </h1>
+      </div>
+      <div className=" grid grid-cols-4 mt-8 gap-5">
+        <div className=" shadow-lg p-3 rounded-xl">
+          <Link to="./">
+            <div className=" w-20 py-3">
+              <img src={cat1} alt="catIcon" />
+            </div>
+            <div>
+              <h1 className=" text-xl font-bold hover:text-gold">
+                Criminal Lawyer
+              </h1>
 
-export default Consultation
+              <p className=" pt-4 text-gray-500 font-medium">
+                Cold, flu, fever, vomiting, infections, headaches or any other
+                general health issues.{" "}
+              </p>
+              <p className=" text-right py-2  animate-pulse font-extrabold  text-gold ">
+                {" "}
+                <ArrowRightAltIcon fontSize="large" />{" "}
+              </p>
+            </div>
+          </Link>
+        </div>
+        <div className=" shadow-lg p-3 rounded-xl">
+          <div className=" w-20 py-3">
+            <img src={cat10} alt="catIcon" />
+          </div>
+          <div>
+            <h1 className=" text-xl font-bold hover:text-gold">
+              Criminal Lawyer
+            </h1>
+
+            <p className=" pt-4 text-gray-500 font-medium">
+              Cold, flu, fever, vomiting, infections, headaches or any other
+              general health issues.{" "}
+            </p>
+            <p className=" text-right py-2  animate-pulse font-extrabold  text-gold ">
+              {" "}
+              <ArrowRightAltIcon fontSize="large" />{" "}
+            </p>
+          </div>
+        </div>
+        <div className=" shadow-lg p-3 rounded-xl">
+          <div className=" w-20 py-3">
+            <img src={cat3} alt="catIcon" />
+          </div>
+          <div>
+            <h1 className=" text-xl font-bold hover:text-gold">
+              Criminal Lawyer
+            </h1>
+
+            <p className=" pt-4 text-gray-500 font-medium">
+              Cold, flu, fever, vomiting, infections, headaches or any other
+              general health issues.{" "}
+            </p>
+            <p className=" text-right py-2  animate-pulse font-extrabold  text-gold ">
+              {" "}
+              <ArrowRightAltIcon fontSize="large" />{" "}
+            </p>
+          </div>
+        </div>
+        <div className=" shadow-lg p-3 rounded-xl">
+          <div className=" w-20 py-3">
+            <img src={cat4} alt="catIcon" />
+          </div>
+          <div>
+            <h1 className=" text-xl font-bold hover:text-gold">
+              Criminal Lawyer
+            </h1>
+
+            <p className=" pt-4 text-gray-500 font-medium">
+              Cold, flu, fever, vomiting, infections, headaches or any other
+              general health issues.{" "}
+            </p>
+            <p className=" text-right py-2  animate-pulse font-extrabold  text-gold ">
+              {" "}
+              <ArrowRightAltIcon fontSize="large" />{" "}
+            </p>
+          </div>
+        </div>
+        <div className=" shadow-lg p-3 rounded-xl">
+          <div className=" w-20 py-3">
+            <img src={cat5} alt="catIcon" />
+          </div>
+          <div>
+            <h1 className=" text-xl font-bold hover:text-gold">
+              Criminal Lawyer
+            </h1>
+
+            <p className=" pt-4 text-gray-500 font-medium">
+              Cold, flu, fever, vomiting, infections, headaches or any other
+              general health issues.{" "}
+            </p>
+            <p className=" text-right py-2  animate-pulse font-extrabold  text-gold ">
+              {" "}
+              <ArrowRightAltIcon fontSize="large" />{" "}
+            </p>
+          </div>
+        </div>
+        <div className=" shadow-lg p-3 rounded-xl">
+          <div className=" w-20 py-3">
+            <img src={cat6} alt="catIcon" />
+          </div>
+          <div>
+            <h1 className=" text-xl font-bold hover:text-gold">
+              Criminal Lawyer
+            </h1>
+
+            <p className=" pt-4 text-gray-500 font-medium">
+              Cold, flu, fever, vomiting, infections, headaches or any other
+              general health issues.{" "}
+            </p>
+            <p className=" text-right py-2  animate-pulse font-extrabold  text-gold ">
+              {" "}
+              <ArrowRightAltIcon fontSize="large" />{" "}
+            </p>
+          </div>
+        </div>
+        <div className=" shadow-lg p-3 rounded-xl">
+          <div className=" w-20 py-3">
+            <img src={cat7} alt="catIcon" />
+          </div>
+          <div>
+            <h1 className=" text-xl font-bold hover:text-gold">
+              Criminal Lawyer
+            </h1>
+
+            <p className=" pt-4 text-gray-500 font-medium">
+              Cold, flu, fever, vomiting, infections, headaches or any other
+              general health issues.{" "}
+            </p>
+            <p className=" text-right py-2  animate-pulse font-extrabold  text-gold ">
+              {" "}
+              <ArrowRightAltIcon fontSize="large" />{" "}
+            </p>
+          </div>
+        </div>
+        <div className=" shadow-lg p-3 rounded-xl">
+          <div className=" w-20 py-3">
+            <img src={cat8} alt="catIcon" />
+          </div>
+          <div>
+            <h1 className=" text-xl font-bold hover:text-gold">
+              Criminal Lawyer
+            </h1>
+
+            <p className=" pt-4 text-gray-500 font-medium">
+              Cold, flu, fever, vomiting, infections, headaches or any other
+              general health issues.{" "}
+            </p>
+            <p className=" text-right py-2  animate-pulse font-extrabold  text-gold ">
+              {" "}
+              <ArrowRightAltIcon fontSize="large" />{" "}
+            </p>
+          </div>
+        </div>
+        <div className=" shadow-lg p-3 rounded-xl">
+          <div className=" w-20 py-3">
+            <img src={cat9} alt="catIcon" />
+          </div>
+          <div>
+            <h1 className=" text-xl font-bold hover:text-gold">
+              Criminal Lawyer
+            </h1>
+
+            <p className=" pt-4 text-gray-500 font-medium">
+              Cold, flu, fever, vomiting, infections, headaches or any other
+              general health issues.{" "}
+            </p>
+            <p className=" text-right py-2  animate-pulse font-extrabold  text-gold ">
+              {" "}
+              <ArrowRightAltIcon fontSize="large" />{" "}
+            </p>
+          </div>
+        </div>
+        <div className=" shadow-lg p-3 rounded-xl">
+          <div className=" w-20 py-3">
+            <img src={womentcat3} alt="catIcon" />
+          </div>
+          <div>
+            <h1 className=" text-xl font-bold hover:text-gold">
+              Criminal Lawyer
+            </h1>
+
+            <p className=" pt-4 text-gray-500 font-medium">
+              Cold, flu, fever, vomiting, infections, headaches or any other
+              general health issues.{" "}
+            </p>
+            <p className=" text-right py-2  animate-pulse font-extrabold  text-gold ">
+              {" "}
+              <ArrowRightAltIcon fontSize="large" />{" "}
+            </p>
+          </div>
+        </div>
+        <div className=" shadow-lg p-3 rounded-xl">
+          <div className=" w-20 py-3">
+            <img src={womentcat2} alt="catIcon" />
+          </div>
+          <div>
+            <h1 className=" text-xl font-bold hover:text-gold">
+              Criminal Lawyer
+            </h1>
+
+            <p className=" pt-4 text-gray-500 font-medium">
+              Cold, flu, fever, vomiting, infections, headaches or any other
+              general health issues.{" "}
+            </p>
+            <p className=" text-right py-2  animate-pulse font-extrabold  text-gold ">
+              {" "}
+              <ArrowRightAltIcon fontSize="large" />{" "}
+            </p>
+          </div>
+        </div>
+        <div className=" shadow-lg p-3 rounded-xl">
+          <div className=" w-20 py-3">
+            <img src={womentcat1} alt="catIcon" />
+          </div>
+          <div>
+            <h1 className=" text-xl font-bold hover:text-gold">
+              Criminal Lawyer
+            </h1>
+
+            <p className=" pt-4 text-gray-500 font-medium">
+              Cold, flu, fever, vomiting, infections, headaches or any other
+              general health issues.{" "}
+            </p>
+            <p className=" text-right py-2  animate-pulse font-extrabold  text-gold ">
+              {" "}
+              <ArrowRightAltIcon fontSize="large" />{" "}
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Consultation;
